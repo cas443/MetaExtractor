3e50333f600faae52c4c738197719dad6e95cd9a
Joanna4983 <Joanna4983@gmail.com>
2018-05-22 14:54:44 +0100

    Small additions

--- base.py
+++ base.py
@@ -1,18 +1,13 @@
-import xml_data_handler
+import xml_meta_handler
 import re
-
-def print_nicely(thingToPrint):
-    for i in thingToPrint:
-        print(i)
 
 
 
 #location = "/home/jo/Desktop/basic_maths.jpg"
-#location = "/home/jo/Desktop/pasta_cooked.jpg"
+location = "/home/jo/Desktop/pasta_cooked.jpg"
 #location = "/home/jo/Desktop/image.jpg"
-location = "/home/jo/Desktop/villa1.JPG"
+#location = "/home/jo/Desktop/villa1.JPG"
 
-#metadata_regex_ord = r"(?<=\\[0-9A-Za-z]{3})([A-Za-z .:\-\/]{3,150}|[0-9]{1,4}:[0-9]{1,4}:[0-9]{1,4})"
 metadata_regex_ord = r"(?<=\\x[0-9A-Za-z]{2})([A-Za-z0-9 ]{1}[A-Za-z0-9 .:#-/]{3,150})(?=\\x)"
 metadata_regex_xml = r"(<\?xpa.*meta>)"
 
@@ -24,17 +19,21 @@ imgdata = str(imgdata)
 
 metadata_ord = []
 metadata_xml = []
+dupes = []
 
-#print(imgdata[30000:60000])
+#print(imgdata[:50000])
 
 print("[+] METADATA FROM FILE: ", re.search(r"(?:.*/)(.*)(?=)", location).group(1))
+
 
 #FINDING ORDINARY METADATA IN IMAGE
 if re.findall(metadata_regex_ord, imgdata):
     for i in re.findall(metadata_regex_ord, imgdata):
-        match = re.search(r"([A-Za-z0-9 .:/-]{8,150})", i)
-        if match:
+        match = re.search(r"([A-Za-z0-9 .:/-]{4,150})", i)
+        if match and match not in dupes:
             metadata_ord.append(match.group(0))
+            dupes.append(match)
+metadata_ord.sort(key=len, reverse=True)
 
 
 #FINDING DATA FROM XML CONTAINED WITHIN THE IMAGE
@@ -42,13 +41,19 @@ if re.findall(metadata_regex_xml, imgdata):
     for i in re.findall(metadata_regex_xml, imgdata):
         metadata_xml.append(i)
 
-xml_meta_touples = xml_data_handler.handle_xml_meta(metadata_xml)
 
-print_nicely(metadata_ord)
+#PRINT ORDINARY DATA
+for i in metadata_ord:
+    print(i)
 
-for key, value in xml_meta_touples.items():
-    if value is not "0" and key is not "WebStatement":
-       print(key, " ", value)
+#print(metadata_xml)
+
+xml_meta_touples = xml_meta_handler.handle_xml_meta(metadata_xml)
+
+#
+# for key, value in xml_meta_touples.items():
+#     if value is not "0" and key is not "WebStatement":
+#        print(key, " ", value)
 
 
 
--- xml_meta_handler.py
+++ xml_meta_handler.py
@@ -7,11 +7,16 @@ def handle_xml_meta(xml): #passed in a list of matches that are/should be xml st
 
     for i in xml:
         #parser_regex = r"(?:.*?:)(.*?=\".*?\")"
-        parser_regex = r"(?:.*?:)(.*?)(?:=\")(.*?)(?:\")" # dictionary
+        #parser_regex = r"(?:.*?:)(.*?)(?:=\")(.*?)(?:\")" # dictionary
+        #parser_regex = r"(?:.*?:.*?:|:.*?:|.*?:)(.*?)(?:=\")(.*?)(?:\")" # dictionary
+        parser_regex = r"(?::)(\w+)(?:=\")(.*?)(?=\")" # dictionary
 
         for j in re.findall(parser_regex, i):
             if j not in dupes:
                 meta_touples.update({j[0]:j[1]})
+                #print(j)
+
+                #meta_touples.update({j[1]:j[2]})
                 dupes.append(j)
 
     return meta_touples
