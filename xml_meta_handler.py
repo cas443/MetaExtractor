097bc94ebe52a79a91c6fa0981232483866913c6
Joanna4983 <Joanna4983@gmail.com>
2018-05-22 16:31:49 +0100

    Minor updates

--- xml_meta_handler.py
+++ xml_meta_handler.py
@@ -11,12 +11,31 @@ def handle_xml_meta(xml): #passed in a list of matches that are/should be xml st
         #parser_regex = r"(?:.*?:.*?:|:.*?:|.*?:)(.*?)(?:=\")(.*?)(?:\")" # dictionary
         parser_regex = r"(?::)(\w+)(?:=\")(.*?)(?=\")" # dictionary
 
+        between_tags = r"(?:>)(\w+.*?)(?:<)"
+        lat_long_regex = r"(?:exif:)([A-Za-z0-9]+)(?:>.*?)([0-9. ]+[A-Z]+)(?:</)"
+        gps_timestamp_regex = r"(?:exif:)([A-Za-z0-9]+)(?:>)([0-9. :]+)(?:</)"
+
         for j in re.findall(parser_regex, i):
             if j not in dupes:
                 meta_touples.update({j[0]:j[1]})
-                #print(j)
-
-                #meta_touples.update({j[1]:j[2]})
                 dupes.append(j)
+
+        for j in re.findall(lat_long_regex, i):
+            if j not in dupes:
+                meta_touples.update({j[0]: j[1]})
+                dupes.append(j)
+
+        for j in re.findall(gps_timestamp_regex, i):
+            if j not in dupes:
+                meta_touples.update({j[0]: j[1]})
+                dupes.append(j)
+
+        # for j in re.findall(between_tags, i):
+        #     if j not in dupes:
+        #         meta_touples.update({j[0]: j[1]})
+        #         dupes.append(j)
+
+        if len(meta_touples) == 0: #fix this, it doesnt work
+            print("[i] NO METADATA FROM XML AVAILABLE.")
 
     return meta_touples
