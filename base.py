7dd1f9e2af40f979e3db6923099f870a902bf786
Joanna4983 <Joanna4983@gmail.com>
2018-05-23 13:12:51 +0100

    The image is displayeed alongside it's metadata

--- base.py
+++ base.py
@@ -1,14 +1,14 @@
 import xml_meta_handler, ord_meta_handler
 import re
-from tkinter.filedialog import askopenfilename
 from tkinter import *
+from PIL import Image, ImageTk
 
 #location = askopenfilename()
 
 #location = "/home/jo/Desktop/basic_maths.jpg"
-location = "/home/jo/Desktop/pasta_cooked.jpg"
+#location = "/home/jo/Desktop/pasta_cooked.jpg"
 #location = "/home/jo/Desktop/image.jpg"
-#location = "/home/jo/Desktop/villa1.JPG"
+location = "/home/jo/Desktop/villa1.JPG"
 #location = "/home/jo/Desktop/510222832.jpg"
 
 file = open(location, "rb")
@@ -19,7 +19,8 @@ imgdata = str(imgdata)
 
 #print(imgdata[:50000])
 
-print("[+] METADATA FROM FILE: ", re.search(r"(?:.*/)(.*)(?=)", location).group(1))
+filename = re.search(r"(?:.*/)(.*)(?=)", imgdata).group(1)
+print("[+] METADATA FROM FILE: ", filename, location)
 
 metadata_ord = ord_meta_handler.handle_meta(imgdata)
 metadata_xml = xml_meta_handler.handle_meta(imgdata)
@@ -41,18 +42,32 @@ for i in metadata_ord:
     all_meta += i + "\n"
     #print(i)
 
-#print(all_meta)
 
 master = Tk()
-scroll = Scrollbar(master)
-text = Text(master, height=30, width=80)
+text0 = Text(master, height=30, width=50)
+text1 = Text(master, height=30, width=80)
+scroll = Scrollbar(master, command=text1.yview)
+image = Image.open(location)
+image = image.resize((300,250), Image.ANTIALIAS)
+image = ImageTk.PhotoImage(image)
+
+
+
+text0.insert(END, "\n")
+text0.image_create(END, image=image)
+text0.pack(side=LEFT)
+
+text1.pack(side=LEFT, fill=Y)
+text1.config(yscrollcommand=scroll.set)
+text1.insert(END, all_meta)
+
 scroll.pack(side=RIGHT, fill=Y)
-text.pack(side=LEFT, fill=Y)
-scroll.config(command=text.yview)
-text.config(yscrollcommand=scroll.set)
-text.insert(END, all_meta)
-# label = Label(master, text=all_meta,)
-# label.pack()
+scroll.config(command=text1.yview)
+
+
+
+
+
 mainloop()
 
 
