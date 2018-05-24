7fd093963b80dba2c8bfba4ea46f36f504dcdfb4
Joanna4983 <Joanna4983@gmail.com>
2018-05-24 10:40:59 +0100

    Adding GUI to image selection

--- base.py
+++ base.py
@@ -1,18 +1,19 @@
 import xml_meta_handler, ord_meta_handler
 import re
 from tkinter import *
+from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 from PIL import Image, ImageTk
 
-# location = askopenfilename()
-# pathfile = str(location)
 
+location = askopenfilename(title="Select image for metadata extraction", filetypes=[("Image Files", "*.jpg")])
 
 #location = "/home/jo/Desktop/basic_maths.jpg"
-location = "/home/jo/Desktop/pasta_cooked.jpg"
+#location = "/home/jo/Desktop/pasta_cooked.jpg"
 #location = "/home/jo/Desktop/image.jpg"
 #location = "/home/jo/Desktop/villa1.JPG"
 #location = "/home/jo/Desktop/510222832.jpg"
+#location = "/home/jo/Desktop/Apple iPhone 4S.jpg"
 
 file = open(location, "rb")
 imgdata = file.read()
@@ -23,7 +24,7 @@ imgdata = str(imgdata)
 #print(imgdata[:50000])
 
 filename = re.search(r"(?:.*/)(.*)(?=)", location).group(1)
-print("[+] METADATA FROM FILE: ", filename, location)
+print("[+] METADATA FROM FILE: ", filename)
 
 metadata_ord = ord_meta_handler.handle_meta(imgdata)
 metadata_xml = xml_meta_handler.handle_meta(imgdata)
@@ -47,26 +48,25 @@ for i in metadata_ord:
 
 
 master = Tk()
+master.title("MetaExtractor")
+
 text0 = Text(master, height=30, width=50)
 text1 = Text(master, height=30, width=80)
 scroll = Scrollbar(master, command=text1.yview)
 image = Image.open(location)
 image = image.resize((300,250), Image.ANTIALIAS)
-i = ImageTk.PhotoImage(image)
-
-
+i = ImageTk.PhotoImage(image, master=master)
 
 text0.insert(END, "\n")
 text0.image_create(END, image=i)
 text0.pack(side=LEFT)
 
-text1.pack(side=LEFT, fill=Y)
 text1.config(yscrollcommand=scroll.set)
 text1.insert(END, all_meta)
+text1.pack(side=LEFT, fill=Y)
 
-scroll.pack(side=RIGHT, fill=Y)
 scroll.config(command=text1.yview)
-
+scroll.pack(side=RIGHT, fill=Y)
 
 
 
