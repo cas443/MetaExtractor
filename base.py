df4c317dfe529eb6ace81ae8558a5aacb8bda0ab
Joanna4983 <Joanna4983@gmail.com>
2018-07-18 15:15:04 +0100

    Added an introduction screen

--- base.py
+++ base.py
@@ -1,5 +1,5 @@
 import xml_meta_handler, ord_meta_handler
-import gui_handler
+import gui_handler, scraper
 
 import re, os
 from tkinter import *
@@ -35,15 +35,47 @@ def start(location):
     gui_handler.gui(metadata_ord, metadata_xml, location, filename)
 
 
-if __name__ == "__main__":
-    #Tk().withdraw() #crates and hides a 'root' lvl window so that the annoying gray window wont be seen
-    #location = askopenfilename(title="Select image for metadata extraction", filetypes=[("Image Files", "*.jpg"), ("Image Files", "*.png")] )
-
+def goto_start():
+    # Tk().withdraw() #crates and hides a 'root' lvl window so that the annoying gray window wont be seen
+    # location = askopenfilename(title="Select image for metadata extraction", filetypes=[("Image Files", "*.jpg"), ("Image Files", "*.png")] )
 
     location = "/home/jo/Desktop/MetaImages/basic_maths.jpg"
-    #location = "/home/jo/Desktop/MetaImages/pasta_cooked.jpg"
+    # location = "/home/jo/Desktop/MetaImages/pasta_cooked.jpg"
 
     start(location)
+
+
+if __name__ == "__main__":
+
+
+    master = Tk()
+    master.title("MetaEx: Metadata Extraction Tool")
+    master.configure(background="#ffffff" )
+    master.pack_propagate(0)
+    master.geometry('400x300')
+
+    mainframe = Frame(master, width=200, height=450, highlightthickness=0, bg="#ffffff")
+    mainframe.grid(row=0, column=0, padx=10, pady=2, sticky=N + S)
+
+    logo = Label(mainframe, text="MetaEx", fg="#00695C", bg="#ffffff", font="Verdana 30 bold", underline=True)
+    logo.grid(row=0, column=0, padx=100, pady=30)
+
+    b = Button(mainframe, text="Open a New Image", command=goto_start)
+    b.grid(row=1, column=0, padx=100, pady=0)
+
+    b2 = Button(mainframe, text="Scrape a Webpage", command=scraper.scrape_url)
+    b2.grid(row=2, column=0, padx=100, pady=10)
+
+    footer = Frame(master, width=200, height=50, highlightthickness=0, bg="#fff3ff")
+    footer.grid(row=1, column=0, padx=10, pady=2, sticky=N + S)
+
+    censor = Label(footer, text="A summer project", fg="#00695C", bg="#ffffff", font="Verdana 8", underline=False)
+    censor.grid(row=1, column=0, padx=100, pady=0, sticky=S)
+
+    master.mainloop()
+
+
+
 
 
 
